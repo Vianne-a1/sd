41d42f6b502ecae4ecae08b2487c0b556cf7de5f
Tiffany Yang <121731114+Vianne-a1@users.noreply.github.com>
2024-10-07 11:30:20 -0400

    Update app.py

--- 13_combine/app.py
+++ 13_combine/app.py
@@ -1,69 +1,30 @@
-# Tiffany Yang
+# Chloe Wong, Tiffany Yang
+# Team Unknown
 # SoftDev
-# Sep 27 2024
+# K13 -- combining past work
+# 2024-9-30
+# Time Spent: 2 hr
 
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template
 import random
 import csv
 
-app = Flask(__name__) #jibili
-
-@app.route("/")
-def hello_world():
-    return "No hablo queso!"
-
-def numbercruncher():
-    list1 = []
-    percentage = []
-    job = []
-    print("the __name__ of this module is... ")
-    print(__name__)
-    
-    # Load the occupations data from CSV
-    with open('occupations.csv', newline='') as csvfile:
-        reader = csv.DictReader(csvfile)
-        for row in reader:
-            list1.append(row)
-    
-    for dict1 in list1:
-        percentage.append(float(dict1.get("Percentage")))
-        job.append(dict1.get("Job Class"))
-
-    # Remove the last entry (which is the "Total")
-    job.pop()
-    percentage.pop()
-
-    # Return the job list and randomly selected job
-    selected_occupation = random.choices(job, weights=percentage)[0]  # extract single occupation
-    return {"selected_occupation": selected_occupation, "job_list": job}
-
-title = "Occupation Selector"
-desc = "Occupations and Random Selector"
-roster = "TNPG + Chloe and Tiffany"
+app = Flask(__name__)
 
 @app.route("/wdywtbwygp")
-def idkwtsf():
-    # Refresh the occupations and selected occupation on each load
-    data = numbercruncher()
-    selected_occupation = data["selected_occupation"]
-    job_list = data["job_list"]
-    
-    return render_template('idkwtsf.html', 
-                           title=title, 
-                           desc=desc, 
-                           roster=roster, 
-                           selected_occupation=selected_occupation, 
-                           job_list=job_list)
+def randomjob():
+    with open('data/occupations.csv', newline='') as csvfile:
+        reader = csv.DictReader(csvfile)
+        rows = list(reader)
+    columns = list(rows[0].keys())
+    jobs = {columns[0]: [], columns[1]: [], columns[2]: []}
+    for row in rows[1:-1]:
+        jobs[columns[0]].append(row[columns[0]])
+        jobs[columns[1]].append(float(row[columns[1]]))
+        jobs[columns[2]].append(row[columns[2]])
+    rand_job = random.choices(jobs[columns[0]], weights=jobs[columns[1]])[0]
+    return render_template('tablified.html', job_info = jobs, categories = columns, job = rand_job)
 
-@app.route("/teams") 
-def team():
-    devos = ["Chloe Wong, "Tiffany Yang"]
-    return render_template('team.html', teamName="Untitled", collection=devos)
-
-@app.route("/my_foist_template") 
-def test_tmplt():
-    coll = [0,1,1,2,3,5,8]
-    return render_template('model_tmplt.html', foo="fooooo", collection=coll)
 
 if __name__ == "__main__":
     app.debug = True
