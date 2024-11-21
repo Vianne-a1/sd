aadee7506399356ad304e80aadf010f4ade93c6c
Tiffany Yang <121731114+Vianne-a1@users.noreply.github.com>
2024-11-21 05:01:20 +0000

    I face rolled to ogard

--- 23_rest/app.py
+++ 23_rest/app.py
@@ -13,22 +13,25 @@ app = Flask(__name__)
 # Your NASA API key
 API_KEY = "Lm0DcjX5IF9ojaCo88NHCv4m5qSUxPGnPd1L5bb2"
 
+
 @app.route("/")
 def home():
     try:
         # Fetch the Astronomy Picture of the Day (APOD) data
-        response = requests.get(f"https://api.nasa.gov/planetary/apod?api_key={API_KEY}")
-        
+        response = requests.get(
+            f"https://api.nasa.gov/planetary/apod?api_key={API_KEY}")
+
         if response.status_code == 200:
             data = response.json()
-            return render_template("apod.html", apod_data=data)
+            return render_template("index.html", apod_data=data)
         else:
             # Handle API errors gracefully
             return f"Error: Unable to fetch data from NASA API (Status Code: {response.status_code})"
-    
+
     except requests.RequestException as e:
         # Handle connection errors
         return f"Error: {e}"
+
 
 # Main entry point
 if __name__ == "__main__":
